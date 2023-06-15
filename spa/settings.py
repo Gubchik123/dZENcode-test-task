ab4c5f401dd73e4ca786c7e8109af134f7104cba
Nikita <nikita.gubarev31@gmail.com>
2023-06-15 12:43:37 +0300

    FEAT: Connect the "comments" app to the project
    Add "comments.apps.CommentsConfig" into the INSTALLED_APPS
    Add comments.urls into the root urlpatterns

--- spa/settings.py
+++ spa/settings.py
@@ -14,12 +14,15 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
+    # Django apps
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # My apps
+    "comments.apps.CommentsConfig",
 ]
 
 MIDDLEWARE = [
