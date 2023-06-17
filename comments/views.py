17711bd67b4f26e82e2707a952ee1cc0992bbd48
Nikita <nikita.gubarev31@gmail.com>
2023-06-17 15:33:23 +0300

    FEAT: Add a CommentCreateView for creating a comment

--- comments/views.py
+++ comments/views.py
@@ -1,6 +1,8 @@
 from typing import Any, NoReturn
+
+from django import http
 from django.views import generic
-from django.http import HttpRequest, HttpResponse, Http404
+from django.contrib import messages
 
 from . import services
 from .models import Comment
@@ -16,11 +18,11 @@ class CommentListView(BaseView, generic.ListView):
     extra_context = {"form": CommentModelForm()}
 
     def get(
-        self, request: HttpRequest, *args: Any, **kwargs: Any
-    ) -> HttpResponse | NoReturn:
+        self, request: http.HttpRequest, *args: Any, **kwargs: Any
+    ) -> http.HttpResponse | NoReturn:
         """Raises "Not found" if ordering is wrong else calls super method."""
         if self.get_ordering() is None:
-            raise Http404
+            raise http.Http404
         return super().get(request, *args, **kwargs)
 
     def get_ordering(self) -> str | None:
@@ -29,3 +31,23 @@ class CommentListView(BaseView, generic.ListView):
             self.request.GET.get("orderby") or "c",
             self.request.GET.get("orderdir") or "desc",
         )
+
+
+class CommentCreateView(generic.CreateView):
+    """View for handling only POST request and creating a comment."""
+
+    model = Comment
+    success_url = "/"
+    http_method_names = ["post"]
+    form_class = CommentModelForm
+
+    def form_valid(self, form: CommentModelForm) -> http.HttpResponse:
+        """Adds the success message and returns the super method."""
+        messages.success(self.request, "Your comment has successfully added.")
+        return super().form_valid(form)
+
+    def form_invalid(self, form: CommentModelForm) -> http.HttpResponse:
+        """Adds form error messages and returns redirect to the success_url."""
+        for error in form.errors.as_data().values():
+            messages.error(self.request, error[0].messages[0])
+        return http.HttpResponseRedirect(self.success_url)
