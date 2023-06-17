501920f2c68483b8da27e247656ee2e2eacbda29
Nikita <nikita.gubarev31@gmail.com>
2023-06-17 14:22:35 +0300

    REFACTOR: Replace the 'get_context_data' method with extra_context
    in the CommentListView

--- comments/views.py
+++ comments/views.py
@@ -13,6 +13,7 @@ class CommentListView(BaseView, generic.ListView):
 
     model = Comment
     paginate_by = 25
+    extra_context = {"form": CommentModelForm()}
 
     def get(
         self, request: HttpRequest, *args: Any, **kwargs: Any
@@ -21,12 +22,6 @@ class CommentListView(BaseView, generic.ListView):
         if self.get_ordering() is None:
             raise Http404
         return super().get(request, *args, **kwargs)
-
-    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
-        """Adds the CommentModelForm to context data and returns it."""
-        context = super().get_context_data(**kwargs)
-        context["form"] = CommentModelForm()
-        return context
 
     def get_ordering(self) -> str | None:
         """Returns ordering string or None by the GET parameters."""
