075835363d2ade7f5fa7e7dedb7823dabc7d7ede
Nikita <nikita.gubarev31@gmail.com>
2023-06-17 15:34:55 +0300

    FEAT: Add url for the CommentCreateView: "add/"
    Add this url for the form in the comment_list.html

--- comments/urls.py
+++ comments/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 
-app_name = "comment"
-urlpatterns = [path("", views.CommentListView.as_view(), name="list")]
+app_name = "comments"
+urlpatterns = [
+    path("", views.CommentListView.as_view(), name="list"),
+    path("add/", views.CommentCreateView.as_view(), name="add"),
+]
