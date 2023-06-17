14bd62c42d94a29c5cb1b171cab4d86b87247c55
Nikita <nikita.gubarev31@gmail.com>
2023-06-17 12:54:59 +0300

    FEAT: Add url for the CommentListView into comments/urls.py

--- comments/urls.py
+++ comments/urls.py
@@ -1,4 +1,7 @@
-from django.urls import path, include
+from django.urls import path
+
+from . import views
 
 
-urlpatterns = []
+app_name = "comment"
+urlpatterns = [path("", views.CommentListView.as_view(), name="list")]
