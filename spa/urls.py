630c4ed4820a84381f44eae802390d16384fbf7d
Nikita <nikita.gubarev31@gmail.com>
2023-06-19 22:32:33 +0300

    FEAT: Remove url to admin panel from the root urlpatterns

--- spa/urls.py
+++ spa/urls.py
@@ -1,5 +1,4 @@
 from django.conf import settings
-from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 
@@ -15,5 +14,4 @@ handler404 = CustomNotFoundView.as_view()
 urlpatterns = [
     path("captcha/", include("captcha.urls")),
     path("", include("comments.urls")),
-    path("admin/", admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
