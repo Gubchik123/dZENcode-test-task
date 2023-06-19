09f4eb54ffeea06479231daf8f61a5be32ae9374
Nikita <nikita.gubarev31@gmail.com>
2023-06-19 22:31:04 +0300

    SETTINGS: Remove useless apps, middlewares and settings
    Remove admin, auth, contenttypes and sessions apps from the INSTALLED_APPS
    Remove Authentication and Session middlewares from the MIDDLEWARE
    Remove AUTH_PASSWORD_VALIDATORS

--- spa/settings.py
+++ spa/settings.py
@@ -19,10 +19,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     # Django apps
-    "django.contrib.admin",
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
     # Third-party apps
@@ -33,10 +29,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
-    "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
@@ -50,9 +44,7 @@ TEMPLATES = [
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
-                "django.template.context_processors.debug",
                 "django.template.context_processors.request",
-                "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
             ],
         },
@@ -71,21 +63,6 @@ DATABASES = {
         "PASSWORD": str(os.getenv("DB_PASSWORD")),
     }
 }
-
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
-    },
-]
 
 LANGUAGE_CODE = "en-us"
 
