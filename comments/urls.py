caf260c11871387a488e1d5528a3dc597cbe1fb5
Nikita <nikita.gubarev31@gmail.com>
2023-06-19 22:33:32 +0300

    FEAT: Remove app_name from comments/urls.py
    Remove this prefix from link to the CommentCreateView in the comment_list.html

--- comments/urls.py
+++ comments/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from . import views
 
 
-app_name = "comments"
 urlpatterns = [
     path("", views.CommentListView.as_view(), name="list"),
     path("add/", views.CommentCreateView.as_view(), name="add"),
